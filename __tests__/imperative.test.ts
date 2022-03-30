46b84bf043f81491891ca32a1b8300dedfae41e3
robertblum-psi <robertblum@phoenixsoftware.com>
2022-03-30 14:04:29 -0700

    V6R1 Apr 2020 Service 7 Patch releases: No code change
                            .--------------.
                            |              |
      **********************| APAR Z120066 |**********************
      *                     |              |                     *
      *                     '--------------'                     *
      * PROBLEM-DESCRIPTION:                                     *
      *                                                          *
      *     Various npm dependencies of the Zowe Imperative,     *
      *     which launches both "zowe ejes log" and the          *
      *     independent CLIs when run under Zowe, was marked     *
      *     as having vulnerabilities that must be resolved.     *
      *     One vulnerability is marked as a breaking change     *
      *     and requires run testing.                            *
      *                                                          *
      * SOLUTION:                                                *
      *                                                          *
      *     Recreate package-lock.json, use https                *
      *     repository, update devDependencies (allow newer      *
      *     release of typedoc and Typescript, add               *
      *     js-tokens).                                          *
      *                                                          *
      * UPDATED-PARTS:                                           *
      *                                                          *
      *     .npmrc, package-lock.json, package.json              *
      *                                                          *
      * WORKAROUND:                                              *
      *                                                          *
      *     Delete package-lock.json and build GitHub            *
      *     projects.                                            *
      *                                                          *
      ************************************************************

--- __tests__/imperative.test.ts
+++ __tests__/imperative.test.ts
@@ -5,8 +5,7 @@
 *
 * SPDX-License-Identifier: EPL-2.0
 *
-* Copyright Contributors to the Zowe Project.
-*
+* Copyright (c) 2020-2021 Phoenix Software International, Inc.
 */
 
 describe("imperative config", () => {
